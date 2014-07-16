e3ff5d44215109cd709414ef3c9003f09bb3f6ee
6i6arka <rado.milushev@gmail.com>
2014-07-16 14:32:33 +0300

    day4 todo-services solved

---
+++ day4 -todo services/bootstrap/src/storage.js
@@ -0,0 +1,23 @@
+/* global TodoApp, localStorage */
+
+TodoApp.provider('storage', function(){
+    var keyName;
+
+    this.setKey = function(key){
+        keyName = key;
+    };
+
+    this.$get = function(){
+        var data = JSON.parse(localStorage.getItem(keyName)) || {};
+
+        return {
+            put: function(key, value){
+                data[key] = value;
+                localStorage.setItem(keyName, JSON.stringify(data));
+            },
+            get: function(key){
+                return data[key];
+            }
+        };
+    };
+});
